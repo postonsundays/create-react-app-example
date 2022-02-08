cdbda2af72625b26a5f421f8022bf3cc02c5aead
SillyScribe95 <98259928+postonsundays@users.noreply.github.com>
2022-02-08 17:46:14 +0000

    another

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import { Button } from 'react-native-elements';
+import React from "react";
+import { Button } from "react-native-elements";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  function onClick() {
+    const data = {
+      title: "afkasd",
+      url: "http://localhost:3000/",
+      text: "onBase",
+    };
+
+    const shareBase = window.navigator.share;
+    console.log(data, "data");
+    if (shareBase) {
+      shareBase(data);
+    }
+  }
+
+  const resso = {
+    onClick,
+    title: "here's how you click react",
+  };
+
   return (
     <>
       <style type="text/css">{`
         @font-face {
           font-family: 'MaterialIcons';
-          src: url(${require('react-native-vector-icons/Fonts/MaterialIcons.ttf')}) format('truetype');
+          src: url(${require("react-native-vector-icons/Fonts/MaterialIcons.ttf")}) format('truetype');
         }
 
         @font-face {
           font-family: 'FontAwesome';
-          src: url(${require('react-native-vector-icons/Fonts/FontAwesome.ttf')}) format('truetype');
+          src: url(${require("react-native-vector-icons/Fonts/FontAwesome.ttf")}) format('truetype');
         }
       `}</style>
 
       <div className="App">
         <header className="App-header">
-          <Button title="I'm a button from React Native Elements" />
+          <Button {...resso} />
         </header>
       </div>
     </>
